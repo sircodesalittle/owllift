59c3b5c940188aa0b7ef43b29c879bf63e448e8e
Alex Dykstra <adykstra1406@gmail.com>
2016-04-08 18:29:57 -0400

    Fixed the issue of exercises being wiped when the exercises are never edited.

--- OwlLift/ActiveExerciseTableViewController.swift
+++ OwlLift/ActiveExerciseTableViewController.swift
@@ -79,6 +79,14 @@ class ActiveExerciseTableViewController: UITableViewController {
         tableView.reloadData()
     }
     
+    func loadExerciseSamples() {
+        let ex1 = Exercise(name: "Bench Press", numSets: 5, numReps: 5, weight: 135, autoIncrement: true)
+        let ex2 = Exercise(name: "Squat", numSets: 5, numReps: 5, weight: 135, autoIncrement: true)
+        
+        exercises!.append(ex1!)
+        exercises!.append(ex2!)
+    }
+    
     func printCompleted() {
         var toSave: HistoricalExercise
         
@@ -87,6 +95,7 @@ class ActiveExerciseTableViewController: UITableViewController {
         }
         else {
             exercises = []
+            loadExerciseSamples()
         }
         
         workouts = loadWorkouts()
